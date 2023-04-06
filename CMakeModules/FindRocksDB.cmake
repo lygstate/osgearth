a73b646e0eadc0888ddfa42d91ce88748dec0ca8
Yonggang Luo <luoyonggang@gmail.com>
2023-04-06 14:56:41 +0800

    Fixes rocksDB

--- CMakeModules/FindRocksDB.cmake
+++ CMakeModules/FindRocksDB.cmake
@@ -40,5 +40,49 @@ find_package_handle_standard_args(ROCKSDB
       "Could NOT find ROCKSDB"
 )
 
+if(ROCKSDB_FOUND)
+	FIND_PACKAGE(ZLIB REQUIRED)
+
+	include(SelectLibraryConfigurations)
+	# Find Snappy library
+	find_library(SNAPPY_LIBRARY_DEBUG NAMES snappyd)
+	find_library(SNAPPY_LIBRARY_RELEASE NAMES snappy)
+	select_library_configurations(SNAPPY)
+	find_package_handle_standard_args(SNAPPY
+		FOUND_VAR
+		  SNAPPY_FOUND
+		REQUIRED_VARS
+		  SNAPPY_LIBRARY
+		FAIL_MESSAGE
+		  "Could NOT find SNAPPY"
+	)
+
+	# Find LZ4 library
+	find_library(LZ4_LIBRARY_DEBUG NAMES lz4d)
+	find_library(LZ4_LIBRARY_RELEASE NAMES lz4)
+	select_library_configurations(LZ4)
+	find_package_handle_standard_args(LZ4
+		FOUND_VAR
+		  LZ4_FOUND
+		REQUIRED_VARS
+		  LZ4_LIBRARY
+		FAIL_MESSAGE
+		  "Could NOT find LZ4"
+	)
+
+	# Find ZSTD library
+	find_library(ZSTD_LIBRARY_DEBUG NAMES zstdd)
+	find_library(ZSTD_LIBRARY_RELEASE NAMES zstd)
+	select_library_configurations(ZSTD)
+	find_package_handle_standard_args(ZSTD
+		FOUND_VAR
+		  ZSTD_FOUND
+		REQUIRED_VARS
+		  ZSTD_LIBRARY
+		FAIL_MESSAGE
+		  "Could NOT find ZSTD_"
+	)
+endif(ROCKSDB_FOUND)
+
 set(ROCKSDB_INCLUDE_DIRS ${ROCKSDB_INCLUDE_DIR} )
 set(ROCKSDB_LIBRARIES ${ROCKSDB_LIBRARY})
